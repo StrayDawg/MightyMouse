e8e74229c1657f19fb41ec24cb29c8768fcfffa4
DaveDaWave <dgobrien@gmail.com>
2026-02-24 12:33:14 -0500

    Add message about locking account

--- config-example.py
+++ config-example.py
@@ -13,7 +13,9 @@ QBITTORRENT_USERNAME = "admin" # qBittorrent username
 QBITTORRENT_PASSWORD = "admin" # qBittorrent password
 
 #Spend Bonus Points?
+# DONATE_TO_POT can lock your account if you don't have yuour password correct, so make sure to set MAM_USER_PASS before setting DONATE_TO_POT to True
 DONATE_TO_POT = False # Automatically donate to the pot once a cycle to get FL wedges (requires Selenium and ChromeDriver, leave False to disable)
+
 BUY_UPLOAD = False # Automatically buy upload (requires Selenium and ChromeDriver, leave False to disable)
 BUY_VIP = False # Automatically try to keep VIP full (requires Selenium and ChromeDriver, leave False to disable)
 
